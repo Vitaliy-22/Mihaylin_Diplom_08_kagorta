d0028fa3379f2a8977a1734523efc157d2422858
Vitaliy-22 <144942660+Vitaliy-22@users.noreply.github.com>
2023-10-17 02:09:25 +0700

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,6 @@
+# URL сервера
+server_url = "https://bede48bf-d97c-4a0a-a9ad-7f35d5b32709.serverhub.praktikum-services.ru"
+# URL API — Создание заказа
+url_create = server_url + "/api/v1/orders"
+# URL API — Получение заказа по номеру трека
+url_get = server_url + "/api/v1/orders/track?t="
